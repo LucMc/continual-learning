edcc0d876fc1a1f6106473833445f2617199dedb
cavlab <lm01065@surrey.ac.uk>
2025-08-04 16:34:14 +0100

    Added inverted pendulum experiments

---
+++ continual_learning_2/experiments/gym_experiments/ipv5/muon.py
@@ -0,0 +1,127 @@
+import time
+from functools import partial
+from pathlib import Path
+from typing import Literal, NamedTuple
+
+import jax
+import jax.experimental
+import jax.flatten_util
+import jax.numpy as jnp
+import tyro
+from chex import dataclass
+from flax.core.scope import DenyList
+from jaxtyping import PRNGKeyArray
+import gymnasium as gym
+from gymnasium.vector import SyncVectorEnv, AsyncVectorEnv
+import numpy as np
+
+from continual_learning_2.configs.envs import EnvConfig
+from continual_learning_2.configs.logging import LoggingConfig
+from continual_learning_2.configs.models import MLPConfig
+from continual_learning_2.configs.optim import MuonConfig, RedoConfig
+from continual_learning_2.configs.rl import PolicyNetworkConfig, PPOConfig, ValueFunctionConfig
+from continual_learning_2.configs.training import RLTrainingConfig
+from continual_learning_2.envs import JittableContinualLearningEnv, get_benchmark
+from continual_learning_2.envs.base import JittableVectorEnv
+from continual_learning_2.models import get_model, get_model_cls
+from continual_learning_2.models.rl import Policy
+from continual_learning_2.optim import get_optimizer
+from continual_learning_2.trainers.continual_rl import JittedContinualPPOTrainer, GymPPOTrainer
+from continual_learning_2.types import (
+    Activation,
+    EnvState,
+    LogDict,
+    Observation,
+    Rollout,
+    StdType,
+)
+from continual_learning_2.utils.buffers import compute_gae_scan
+from continual_learning_2.utils.monitoring import (
+    Logger,
+    accumulate_concatenated_metrics,
+    explained_variance,
+    get_logs,
+    prefix_dict,
+    pytree_histogram,
+)
+from continual_learning_2.utils.training import TrainState
+
+@dataclass(frozen=True)
+class Args:
+    seed: int = 42
+    wandb_mode: Literal["online", "offline", "disabled"] = "online"
+    wandb_project: str | None = None
+    wandb_entity: str | None = None
+    data_dir: Path = Path("./experiment_results")
+    resume: bool = False
+
+
+def muon_gym_invertedpendulum_experiment() -> None:
+    args = tyro.cli(Args)
+
+    if args.wandb_mode != "disabled":
+        assert args.wandb_project is not None
+        assert args.wandb_entity is not None
+
+    optim_conf = MuonConfig(learning_rate=1e-4)
+
+    start = time.time()
+    trainer = GymPPOTrainer(
+        env_id="InvertedPendulum-v5",
+        seed=args.seed,
+        ppo_config=PPOConfig(
+            policy_config=PolicyNetworkConfig(
+                optimizer=optim_conf,
+                network=MLPConfig(
+                    num_layers=2,
+                    hidden_size=64,
+                    output_size=1,  # InvertedPendulum-v5 has 1 continuous action
+                    activation_fn=Activation.Tanh,
+                    kernel_init=jax.nn.initializers.lecun_normal(),
+                    dtype=jnp.float32,
+                ),
+                std_type=StdType.MLP_HEAD,  # For continuous control
+            ),
+            vf_config=ValueFunctionConfig(
+                optimizer=optim_conf,
+                network=MLPConfig(
+                    num_layers=2,
+                    hidden_size=64,
+                    output_size=1,
+                    activation_fn=Activation.Tanh,
+                    kernel_init=jax.nn.initializers.lecun_normal(),
+                    dtype=jnp.float32,
+                ),
+            ),
+            num_rollout_steps=2048,
+            num_epochs=10,
+            num_gradient_steps=32,
+            gamma=0.99,
+            gae_lambda=0.95,
+            entropy_coefficient=0.01,
+            clip_eps=0.2,
+            vf_coefficient=0.5,
+            normalize_advantages=True,
+        ),
+        train_cfg=RLTrainingConfig(
+            resume=False,
+            steps_per_task=1_000_000,
+        ),
+        logs_cfg=LoggingConfig(
+            run_name=f"muon_{args.seed}",
+            wandb_entity=args.wandb_entity,
+            wandb_project=args.wandb_project,
+            group="gym_invertedpendulum",
+            save=False,  # Disable checkpoints cause it's so fast anyway
+            wandb_mode=args.wandb_mode
+        ),
+        num_envs=32,
+        async_envs=True,
+    )
+    
+    trainer.train()
+
+    print(f"Training time: {time.time() - start:.2f} seconds")
+
+if __name__ == "__main__":
+    muon_gym_invertedpendulum_experiment()
