16771cfa67d96fd2212cfaec6a8083cd8929b622
rain <evan@latent.dev>
2025-09-22 02:54:13 +0100

    Add slipperycheetah

--- continual_learning/envs/slippery_mujoco.py
+++ continual_learning/envs/slippery_mujoco.py
@@ -8,6 +8,7 @@ import numpy as np
 from brax import envs as brax_envs
 from brax.envs.ant import Ant
 from brax.envs.humanoid import Humanoid
+from brax.envs.half_cheetah import Halfcheetah
 from brax.envs.base import PipelineEnv, State
 from brax.io import mjcf
 from etils import epath
@@ -154,6 +155,48 @@ class SlipperyAnt(Ant):
 
         if self._use_contact_forces:
             raise NotImplementedError("use_contact_forces not implemented.")
+
+
+class SlipperyCheetah(Halfcheetah):
+    """The Humanoid environment but with customisable friction"""
+
+    def __init__(
+        self,
+        friction: float,
+        forward_reward_weight=1.0,
+        ctrl_cost_weight=0.1,
+        reset_noise_scale=0.1,
+        exclude_current_positions_from_observation=True,
+        backend="generalized",
+        **kwargs,
+    ):
+        #### This whole loop is copypasted from the parent class, except
+        path = epath.resource_path("brax") / "envs/assets/half_cheetah.xml"
+        sys = mjcf.load(path)
+
+        #### We set the friction programmatically
+        model = sys.mj_model
+        model.geom_friction[:] = np.array([friction, 0.1, 0.1])
+        sys = sys.replace(mj_model=model)
+
+        n_frames = 5
+
+        if backend in ["spring", "positional"]:
+            sys = sys.tree_replace({"opt.timestep": 0.003125})
+            n_frames = 16
+            gear = jnp.array([120, 90, 60, 120, 100, 100])
+            sys = sys.replace(actuator=sys.actuator.replace(gear=gear))
+
+        kwargs["n_frames"] = kwargs.get("n_frames", n_frames)
+
+        super().__init__(sys=sys, backend=backend, **kwargs)
+
+        self._forward_reward_weight = forward_reward_weight
+        self._ctrl_cost_weight = ctrl_cost_weight
+        self._reset_noise_scale = reset_noise_scale
+        self._exclude_current_positions_from_observation = (
+            exclude_current_positions_from_observation
+        )
 
 
 class JittableVectorEnvWrapper(JittableVectorEnv):
@@ -322,4 +365,65 @@ class ContinualHumanoid(JittableContinualLearningEnv):
     @property
     def action_dim(self) -> int:
         env = SlipperyHumanoid(friction=self.frictions[0])
+        return env.action_size
+
+
+class ContinualCheetah(JittableContinualLearningEnv):
+    def __init__(self, seed: int, config: EnvConfig):
+        self._num_envs = config.num_envs
+        self._episode_length = config.episode_length
+
+        rng = np.random.default_rng(seed)
+        self.seed = seed
+        low, high = np.log10(0.02), np.log10(2.0)
+        self.frictions = np.pow(10, rng.uniform(low=low, high=high, size=config.num_tasks))
+        self.current_task = 0
+        self.saved_envs: JittableVectorEnv | None = None
+        self.reward_gain = 1.0
+        self.backend = "mjx"
+
+    @property
+    def tasks(self) -> Generator[JittableVectorEnv, None, None]:
+        for task in range(self.current_task, len(self.frictions)):
+            self.current_task = task
+            yield self._get_task(task, self.saved_envs)
+            self.saved_envs = None
+
+    def save(self, env_state: EnvState) -> dict:
+        return {"current_task": self.current_task, "env_state": env_state}
+
+    def load(self, checkpoint: dict):
+        self.current_task = checkpoint["current_task"]
+        self.saved_env_state = checkpoint["env_state"]
+
+    def _get_task(self, task_id: int, env_checkpoint: EnvState) -> JittableVectorEnv:
+        friction = self.frictions[task_id]
+        return self._make_envs(friction, env_checkpoint)
+
+    def _make_envs(self, friction: float, env_checkpoint: EnvState) -> JittableVectorEnv:
+        return JittableVectorEnvWrapper(
+            seed=self.seed,
+            env=SlipperyAnt(friction=friction, backend=self.backend),
+            num_envs=self.num_envs,
+            episode_length=self._episode_length,
+            env_checkpoint=env_checkpoint,
+            reward_gain=self.reward_gain,
+        )
+
+    @property
+    def num_envs(self) -> int:
+        return self._num_envs
+
+    def evaluate(self, agent: Agent, forgetting: bool = False) -> dict[str, float] | None:
+        del agent, forgetting
+        return None
+
+    @property
+    def observation_spec(self) -> jax.ShapeDtypeStruct:
+        env = SlipperyAnt(friction=self.frictions[0])
+        return jax.ShapeDtypeStruct((1, env.observation_size), jnp.float32)
+
+    @property
+    def action_dim(self) -> int:
+        env = SlipperyAnt(friction=self.frictions[0])
         return env.action_size
