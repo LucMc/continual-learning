577d78ef8fe6fa7c4e46fd455e05b90f2de52826
Luc <lucmccutcheon.home@gmail.com>
2026-05-08 13:52:40 +0100

    Minor settings change td ant

--- experiments/batch_runs/slurm_td_ant_const.sh
+++ experiments/batch_runs/slurm_td_ant_const.sh
@@ -9,7 +9,7 @@
 #SBATCH --time=02-23:00:00
 #SBATCH -o slurm.%N.%j.out
 #SBATCH -e slurm.%N.%j.err
-#SBATCH --array=0-11   # 6 algos x 2 seeds = 12 jobs
+#SBATCH --array=0-5   # 6 algos x 2 seeds = 12 jobs
 
 # Time-delayed Ant constant-per-task sweep on Surrey aisurrey.
 #
@@ -37,7 +37,7 @@ VENV_DIR="../../../.venv"
 script_name="${1:-td_ant.py}"
 
 algos=("adam" "regrama" "cpr" "redo" "cbp" "shrink_and_perturb")
-seeds=(0 1)
+seeds=(0)
 
 num_algos=${#algos[@]}
 num_seeds=${#seeds[@]}
@@ -53,8 +53,8 @@ output_filename="td_ant_const_${SLURM_ARRAY_TASK_ID}_${algo}_seed_${seed}.out"
 overall_obs=9
 overall_act=9
 delay_mode="task_boundary_constant"
-delay_info_mode="none"
-num_tasks=40
+delay_info_mode="blind"
+num_tasks=100
 steps_per_task=20000000
 num_envs=2048
 
